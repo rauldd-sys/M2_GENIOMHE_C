1451cfa83b5f12534b701a93ce17e6cbcc7567f7
Raul Duran De Alba <rda2799@gmail.com>
2025-09-22 13:56:59 +0200

    test

---
+++ exercise/LinkedList.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+typedef struct LinkedList {
+    Node* head;
+} LinkedList;
+LinkedList* createLinkedList() {
+    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
+    list->head = NULL;
+    return list;
+}
+void append(LinkedList* list, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (list->head == NULL) {
+        list->head = newNode;
+    } else {
+        Node* temp = list->head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void display(LinkedList* list) {
+    Node* temp = list->head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+void freeLinkedList(LinkedList* list) {
+    Node* temp = list->head;
+    while (temp != NULL) {
+        Node* next = temp->next;
+        free(temp);
+        temp = next;
+    }
+    free(list);
+}
+int main() {
+    LinkedList* list = createLinkedList();
+    append(list, 10);
+    append(list, 20);
+    append(list, 30);
+    display(list);
+    freeLinkedList(list);
+    return 0;
+}
